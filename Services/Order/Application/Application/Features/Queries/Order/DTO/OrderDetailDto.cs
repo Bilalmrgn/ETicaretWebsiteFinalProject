c1e96cdae96f9e1935e81b020895291ce133855b
bilalmrgn <bilal.emir.mergen@hotmail.com>
2026-01-28 03:43:17 +0300

    revision

---
+++ Services/Order/Application/Application/Features/Queries/Order/DTO/OrderDetailDto.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Application.Features.Queries.Order.DTO
+{
+    //bu sınıfı oluşturmamın amacı şu:
+    /*benim OrderQueryResponse nesnemde Order details var yani ben aslında orderDetails i order ın içerisinde göstermek istiyorum.
+     Bu yüzden de OrderDetails kısmını QueryHandler da map lemem gerekiyor. Bu yüzden domain deki orderDetail i kullanmayacağıma göre
+    ekstra bir tane sınıf oluştururum ve Dto olur ve bu sınıfı map lemek için kullanırım
+     */
+    public class OrderDetailDto
+    {
+        public int OrderDetailId { get; set; }
+        public string ProductId { get; set; }
+        public int ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductAmount { get; set; }
+        public decimal ProductTotalPrice { get; set; }
+        //public int OrderingId { get; set; }
+        //public Ordering Ordering { get; set; }
+        //yukarıdaki iki nesneye gerek yok çünkü zaten üstte Order duracak bir daa burada yazmanın mantığı yok
+
+    }
+}
---
+++ Services/Order/Application/Application/Features/Queries/Order/GetAllOrder/GetAllOrderQueryResponse.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Order.Application.Features.Queries.Order.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Application.Features.Queries.Order.GetAllOrder
+{
+    public class GetAllOrderQueryResponse
+    {
+        public int OrderingId { get; set; }
+        public string UserId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderDetailDto> OrderDetails { get; set; }
+    }
+}
